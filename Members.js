853a7e118a4a43c465b9ed9efae7b28b87c219f5
Deary <chickenflakes9@gmail.com>
2019-04-10 19:30:13 +0700

    First Initialize

---
+++ Members.js
@@ -0,0 +1,12 @@
+const members = [
+  {
+    id: 1,
+    name: 'Deary'
+  },
+  {
+    id: 2,
+    name: 'Ketibs'
+  }
+];
+
+module.exports = members;
---
+++ routes/api/members.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const uuid = require('uuid');
+const router = express.Router();
+const members = require ('../../Members');
+
+// Gets all members
+router.get('/', (req, res) => res.json(members));
+
+// Get Single members
+router.get('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found) {
+      res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `Member not found with the id ${req.params.id}` });
+  }
+});
+
+// Create member
+router.post('/', (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name
+  }
+
+  if(!newMember.name ) {
+    return res.status(400).json({ msg: 'Please include name' });
+  }
+
+  members.push(newMember);
+  res.json(members);
+  // res.redirect('/');
+});
+
+// Update members
+router.put('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found) {
+      const updMember = req.body;
+      members.forEach(member => {
+        if (member.id === parseInt(req.params.id)) {
+          member.name = updMember.name ? updMember.name : member.name;
+
+          res.json({ msg: 'Member updated', member });
+        }
+      });
+  } else {
+    res.status(400).json({ msg: `Member not found with the id ${req.params.id}` });
+  }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if(found) {
+      res.json({
+      msg: 'Member deleted',
+      members:members.filter(member => member.id !== parseInt(req.params.id))
+    });
+  } else {
+    res.status(400).json({ msg: `Member not found with the id ${req.params.id}` });
+  }
+});
+
+
+module.exports = router;
